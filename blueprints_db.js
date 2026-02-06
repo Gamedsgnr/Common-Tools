9c963fa05751fb3b3996230585a7a1c5603af997
Teodor Artemiev <53268017+Gamedsgnr@users.noreply.github.com>
2026-02-06 15:18:33 +0100

    Upload Shader view and Blueprints view

---
+++ blueprints_db.js
@@ -0,0 +1,12 @@
+const blueprintsData = [
+    {
+        "id": "bp_example_01",
+        "title": "Пример: Wasted Logic",
+        "desc": "Логика смерти персонажа с Ragdoll.",
+        // ID из ссылки blueprintue.com/render/7m8ees6k/
+        "embedId": "7m8ees6k", 
+        // Сюда вставляется огромная простыня текста из Unreal
+        "rawCode": `Begin Object Class=/Script/BlueprintGraph.K2Node_Event Name="K2Node_Event_0"...` 
+    }
+    // Сюда будете добавлять новые блоки через админку
+];
---
+++ shaders_db.js
@@ -0,0 +1,9 @@
+const shadersData = [
+    {
+        "id": "sh_ice_material",
+        "title": "Ice Shader (Translucent)",
+        "desc": "Полупрозрачный лед с фейковым преломлением (S&S).",
+        "embedId": "placeholder_id", // ID с сайта BlueprintUE
+        "rawCode": `Begin Object Class=/Script/Engine.Material Name="M_Ice"...` 
+    }
+];
